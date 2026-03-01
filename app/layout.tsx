5800031e268c7fb63fc3eb43a0640cfbce46c4d6
Lou Verde <louverde@hotmail.com>
2026-03-01 10:41:16 -0500

    feat: update LIFE logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/images/logo.png",
-        width: 1200,
-        height: 630,
+        width: 800,
+        height: 533,
       },
     ],
   },
